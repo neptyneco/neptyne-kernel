fbeb9af04124b0c6dd24c6465350e4daa3b26611
Jack Amadeo <jamadeo@gmail.com>
2024-08-06 10:43:34 -0400

    Apply import patch

--- neptyne_kernel/get_ipython_mockable.py
+++ neptyne_kernel/get_ipython_mockable.py
@@ -3,4 +3,7 @@ from IPython import InteractiveShell
 
 def get_ipython_mockable() -> InteractiveShell:
     # This exists so we can mock it in unit tests
-    return get_ipython()  # type: ignore  # noqa: F821
+    try:
+        return get_ipython()  # type: ignore  # noqa: F821
+    except NameError:
+        return None
